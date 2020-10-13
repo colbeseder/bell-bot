47b0475af27cbab1b67bcceed8cf0e16bcb4c747
Bryan <bryanjappleby@gmail.com>
2020-10-13 18:05:07 +0300

    debug startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,17 @@
-/opt/python/3.8.3/bin/python3.8 -m pip install -r requirements.txt
-/opt/python/3.8.3/bin/python3.8 serve.py
+#/bin/bash
+
+echo "start bell setup"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo current $DIR
+
+python -m pip install -r requirements.txt
+
+
+export FLASK_APP=serve.py
+gunicorn --bind=0.0.0.0 --timeout 600 "serve.py"
+
+echo "end bell setup"
+
+python serve.py
